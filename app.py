e56b8ff14f0a7df94dd0b0d2ac1bb909c383f98a
slovelii <susanamarinurrego016@gmail.com>
2026-09-10 13:37:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,5 +42,11 @@ with st.expander('Analizar texto'):
             st.lottie(animation, width=350)
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
+            with open("Sad Face.json") as source:
+              animation=json.load(source)
+            st.lottie(animation, width=350)
         else:
             st.write( 'Es un sentimiento Neutral 😐')
+            with open("Neutral face.json") as source:
+              animation=json.load(source)
+            st.lottie(animation, width=350)
