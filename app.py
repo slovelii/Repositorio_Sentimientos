e51ebddcd47c8aba322dd5100d1d3a9b792f2a3b
slovelii <susanamarinurrego016@gmail.com>
2026-09-10 13:15:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,10 @@ with st.expander('Analizar texto'):
         st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
-            st.write( 'Es un sentimiento Positivo 😊')
+            st.write( 'Es un sentimiento Positivo 😊')    
+            with open("Feliz.json") as source:
+              animation=json.load(source)
+            st.lottie(animation, width=350)
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
         else:
